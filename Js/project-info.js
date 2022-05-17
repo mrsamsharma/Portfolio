59f7efcf06480ac2621956eee8228b596cdd2c79
Samuel Felice <samwfelice@gmail.com>
2022-05-17 20:12:18 +0530

    add temp html for window popup

---
+++ Js/project-info.js
@@ -0,0 +1,16 @@
+//Project One Image
+function automaticElements(value) {
+    let heading = document.createElement('h2');
+    heading.className = '';
+}
+
+
+
+
+let project1 = [{
+    name: 'Tonic',
+    subheading: 'CANOPY • Back End Dev • 2015',
+    featuredimage: './images/cards/card1.svg',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    technologies: '',
+}]
